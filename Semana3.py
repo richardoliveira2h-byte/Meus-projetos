d58d186b279e13a56ebd29608646002981293664
richardoliveira2h-byte <richardoliveira2h@gmail.com>
2025-08-14 16:04:53 -0300

    Create Semana3.py

---
+++ Semana3.py
@@ -0,0 +1,11 @@
+import math
+
+ld = 8
+r3 = 1.732
+
+resultado = (3*r3/2) * (ld**2)
+
+arredondamento = round(resultado, 2)
+print('A área do hexágono é de :',arredondamento ,'centimetros ao quadrado.')
+
+
