2deb0df6b87f429ea1849db68fbd8cffcf96db25
richardoliveira2h-byte <richardoliveira2h@gmail.com>
2025-08-18 16:31:47 -0300

    Create Atividade 3.py

---
+++ Projetos/Semana 3/Atividade 3.py
@@ -0,0 +1,10 @@
+# Função que soma os elementos de uma lista
+def soma_lista(lista):
+    total = 0
+    for num in lista:
+        total += num  # Acumula cada valor na variável 'total'
+    return total  # Retorna a soma total
+
+# Chamada da função com a lista [1, 2]
+lista = [1, 2]
+print(soma_lista(lista))  # Saída será 3
