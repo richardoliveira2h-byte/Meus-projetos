d51d0b19a00ef56d66338bc2f3bd5c4c4087f27d
richardoliveira2h-byte <richardoliveira2h@gmail.com>
2025-08-18 17:08:30 -0300

    Update Atividade 4.py
    Por fim, ele pega uma tupla com nomes em letras minúsculas e cria uma nova tupla com esses nomes convertidos para letras maiúsculas, mostrando o resultado.
    
    Tudo isso ajuda a entender operações matemáticas, manipulação de listas e tuplas, além de como criar e usar funções simples em Python.

--- Projetos/Semana 3/Atividade 4.py
+++ Projetos/Semana 3/Atividade 4.py
@@ -1,6 +1,6 @@
 # Tupla com nomes minúsculos
 nomes = ('alice', 'bob', 'lucas')
-
+ 
 # Cria uma nova tupla com os nomes convertidos para letras maiúsculas
 nomes_maiusculo = tuple(nome.upper() for nome in nomes)
 
